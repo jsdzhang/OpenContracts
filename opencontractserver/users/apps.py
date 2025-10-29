3012ab1fc89325d8dbb90b1ed063db60a78962a0
JSv4 <scrudato@umich.edu>
2025-10-28 21:53:10 -0400

    Updated posthog to use singlelton and reduce logging overhead / latency.

--- opencontractserver/users/apps.py
+++ opencontractserver/users/apps.py
@@ -7,6 +7,14 @@ class UsersConfig(AppConfig):
     verbose_name = _("Users")
 
     def ready(self):
+        import posthog
+        from django.conf import settings
+
+        # Initialize PostHog globally as per official Django integration
+        if settings.TELEMETRY_ENABLED:
+            posthog.api_key = settings.POSTHOG_API_KEY
+            posthog.host = settings.POSTHOG_HOST
+
         try:
             import opencontractserver.users.signals  # noqa F401
         except ImportError:
